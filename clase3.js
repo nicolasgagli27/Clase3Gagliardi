30e7e09cac13291c90a96bf6b919f9da4d800ba1
nicolasgagli27 <nicolasgagli@yahoo.com.ar>
2021-11-06 09:17:03 -0300

    Clase 3

---
+++ clase3.js
@@ -0,0 +1,18 @@
+
+for (let i = 0 ; i < 5 ; i = i + 1){
+    let clubes = prompt ("Ingrese club");
+    let titulos = prompt ("Ingrese cantidad de titulos");
+
+
+if (titulos >=15){
+
+    console.log (" El club " + clubes);
+    console.log ("Es un club ganador");
+}
+
+else{
+    console.log (" El club " + clubes);
+    console.log ("No es un club ganador");
+}
+
+}
